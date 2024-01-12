dc13c7c9c5d7ed2378572426cb4a08c5a1846263
CK861 <155629610+CK861@users.noreply.github.com>
2024-01-12 11:15:03 +0000

    08 Funtionen 03

--- src/08_funktionen_1.js
+++ src/08_funktionen_1.js
@@ -34,12 +34,50 @@ function ausgabeNamen()
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
 
-ausgabeNamen2("Maria"); // ARGUMENT DATEN FÜR PARAMETER
-ausgabeNamen2("Mary Jane"); 
+// ausgabeNamen2("Maria"); // ARGUMENTE SIND DATEN FÜR PARAMETER
+// ausgabeNamen2("Mary Jane"); 
 
 function ausgabeNamen2(firstName) //PARAMETER
 {
 
     console.log("Hallo, " + firstName +"!");  
+
+}
+
+
+/***** Funktionen 02c *****/
+////MEHRERE PARAMETER/////
+
+
+// ausgabeNamenParams("Max","Mütze");
+
+// const prompt = require('prompt-sync')({sigint: true});
+
+// ausgabeNamenParams(prompt ("Bitte Vornamen eingeben: "),prompt("Bitte Nachnamen eingeben: "));  //PIPING
+
+function ausgabeNamenParams(firstName, familyName) //PARAMETER
+{
+
+    console.log("Hallo, " + firstName + " " + familyName + "!");  
+
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenSRP("max","mütze");
+
+function ausgabeNamenSRP(firstName, familyName) //PARAMETER
+{
+
+    // 1. Funktionalität: string composing
+const GAP = " ";
+let outputStr = "Hallo, " + firstName + GAP + familyName + "!"
+
+    // 2. Funktionalität: string output
+
+    console.log(outputStr);  
 
 }

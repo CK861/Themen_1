79be2012382949c408d363520dc98898b694aa34
CK861 <155629610+CK861@users.noreply.github.com>
2024-01-15 09:50:04 +0000

    09 divide variante 1

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,9 +10,40 @@
 4. Ausgabe in Konsole : check!
 */
 
-// module : substraction a - b / test:
+// module: divison a / b
 
-// output(substract(4,3))
+// output(div(4,2));
+// output(div(4,2));
+// output(div(3,2));
+// output(div(3,-2));
+// output(div(0,2));
+// output(div(0,0));
+
+function div(a,b)
+{
+	if (b !== 0) // b ungleich 0
+	{
+		return a / b;
+	}
+	else 
+	{
+		return "Div durch Null ist nicht definiert";
+	}	
+}
+
+
+// module: multiplikation a * b
+
+// output(multiply(3,2));
+
+function multiply(a,b)
+{
+	return a * b;	
+}
+
+// module: substraction a - b / test:
+
+// output(substract(4,3));
 
 function substract(a,b)
 {

51c03661f5a7d9255d350b6c99afdd3b0b15f8f0
CK861 OK <ck.comcave@proton.me>
2024-01-19 08:30:25 +0000

    14 OBJEKTE

--- src/14_objects.js
+++ src/14_objects.js
@@ -2,44 +2,75 @@
 
 
 
-let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);
-output("------------");
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
 
-// Modul: Ausgabe | Test
-// output("hi");
-function output(outputData) {
-    console.log(outputData);
-}
+// // Modul: Ausgabe | Test
+// // output("hi");
+// function output(outputData) {
+//     console.log(outputData);
+// }
 
-// // nested arrays - Prinzip: [[[]]]
-// // nested arrays  - hier: [[],[]]
-arr =   [
-                 ["Ich","bin","Max"],
-                 ["Ich","bin","Moritz"]
-         ];
-
-
-output(arr);
-output(arr[0]);
-output(arr[0][0]+ " " + arr[1][2]);
-output(arr[1][0]);
-output("------------");
+// // // nested arrays - Prinzip: [[[]]]
+// // // nested arrays  - hier: [[],[]]
+// arr =   [
+//                  ["Ich","bin","Max"],
+//                  ["Ich","bin","Moritz"]
+//          ];
 
 
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
 
-// nested loop
 
-for (let i = 0; i < arr.length; i++) {
 
-    for (let j = 0; j < arr[i].length; j++) {
+// // nested loop
 
-        output("Index i: " + i + " Index j: " + j);
+// for (let i = 0; i < arr.length; i++) {
 
-        output(arr[i][j]);      
+//     for (let j = 0; j < arr[i].length; j++) {
 
+//         output("Index i: " + i + " Index j: " + j);
+
+//         output(arr[i][j]);      
+
+//     }
+
+// }     
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+
+let person = 
+{
+    firstName:"Behrang",
+    familyName:"Valadkhani",
+    salary: [120000, 160000],
+    permission: true,
+    sayHello: function()
+    {
+        return "HELLLO";
     }
+};
 
-}      
+output(person);
+output(person.familyName)
+output(person.firstName)
+output(person.salary[1])
+output(person.permission)
+output(person.sayHello())
+
+const text = person.sayHello() + ", ich bin " + person.firstName + 
+             " und verdiene " + person.salary[1] + "p.a."
+
+output(text)
+
+
+function output(outputData) {
+    console.log(outputData);}
